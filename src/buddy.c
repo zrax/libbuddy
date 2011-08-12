fb8bbc9911c42a210774624264e2742ab4dda027
Francesc Gordillo i Cortínez <frangor@frangor.info>
2011-08-12 04:02:44 +0200

    New mail on copyright information.
    Signed-off-by: Francesc Gordillo i Cortínez <frangor@gmx.es>

--- src/buddy.c
+++ src/buddy.c
@@ -2,7 +2,7 @@
 
 /*      buddy.c
  *
- *      Copyright 2010 Francesc Gordillo <frangor+dev@gmail.com>
+ *      Copyright 2011 Francesc Gordillo i Cortínez <frangor@gmx.es>
  *
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
