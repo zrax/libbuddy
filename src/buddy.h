024472e7569f43855e76cb997f11b954ca7e4300
Francesc Gordillo i Cortínez <frangor@frangor.info>
2011-12-23 15:49:06 +0100

    New mail contact.
    Signed-off-by: Francesc Gordillo i Cortínez <frangor@lavabit.com>

--- src/buddy.h
+++ src/buddy.h
@@ -2,7 +2,7 @@
 
 /*      buddy.h
  *
- *      Copyright 2011 Francesc Gordillo i Cortínez <frangor@gawab.com>
+ *      Copyright 2011 Francesc Gordillo i Cortínez <frangor@lavabit.com>
  *
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
